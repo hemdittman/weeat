21cc25223b131a1070804b9458806fe8ba609032
hemdat.dittman <hemdat.dittman@il-017239-mbp.local>
2018-03-21 18:50:03 +0200

    cuisine controller, removed unused tests

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,2 +1,52 @@
 class CuisinesController < ApplicationController
+  before_action :query_cuisine, only: [:show, :update, :destroy]
+
+  def index
+    @cuisines = Cuisine.all
+    render json: @cuisines
+  end
+
+  def show
+    if @cuisine
+      render json: @cuisine
+    else
+      render json: {error: "not-found"}, status: 404
+    end
+  end
+
+  def create
+    @cuisine = Cuisine.new(rest_params)
+    if @cuisine.save
+      render json: @cuisine
+    else
+      render json: @cuisine.errors.messages
+    end
+  end
+
+  def update
+    if @cuisine.update(rest_params)
+      render json: @cuisine
+    else
+      render json: @cuisine.errors.messages
+    end
+  end
+
+  def destroy
+    if @cuisine.destroy
+      head :ok
+    else
+      render json: @cuisine.errors.messages
+    end
+  end
+
+  private
+
+  def rest_params
+    params.permit(:name)
+  end
+
+  def query_cuisine
+    @cuisine = Cuisine.where(id: params[:id]).first
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,5 +3,4 @@ class Restaurant < ApplicationRecord
   belongs_to :cuisine
 
   validates :name, :address, :accepts_10bis, :max_delivery_time_minutes, presence: true
-
 end
