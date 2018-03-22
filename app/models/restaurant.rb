ca49a284f90c3744eb9bc305b5e390a85f7c9f62
hemdat.dittman <hemdat.dittman@il-017239-mbp.local>
2018-03-22 10:49:31 +0200

    cosmetics

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ApplicationRecord
 
+  validates :name, :address, :accepts_10bis, :max_delivery_time_minutes, presence: true
+
   belongs_to :cuisine
 
-  validates :name, :address, :accepts_10bis, :max_delivery_time_minutes, presence: true
 end
